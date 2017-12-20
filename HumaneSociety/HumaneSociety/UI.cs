2d024016635710c694f3da5814413cbeaabbe8e4
Corey Rome <coreyrome2@gmail.com>
2017-12-20 11:21:57 -0600

    added UI class from solo project

--- HumaneSociety/HumaneSociety/UI.cs
+++ HumaneSociety/HumaneSociety/UI.cs
@@ -7,6 +7,62 @@ using System.Threading.Tasks;
 namespace HumaneSociety
 {
     class UI
-    {
+    {      
+            private string welcomeChoice;
+            private string password;
+            private string empPassword;
+
+            public UI()
+            {
+                empPassword = "PawPrint22";
+            }
+            public string Welcome()
+            {
+                Console.WriteLine("Welcome to Pawlicious Puppy Love, the Humane Society that can meet all of your needs!");
+                Console.WriteLine("Are you a customer or an employee?");
+                return welcomeChoice = Console.ReadLine();
+            }
+
+            public void CustEmployeeSwitch()
+            {
+                try
+                {
+                    if (welcomeChoice == "adopter")
+                    {
+                        //AdopterPath();
+                    }
+                    else if (welcomeChoice == "employee")
+                    {
+                        PasswordPath();
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("That was not an option, try again. Check caps lock as all user input is case sensitive.");
+                    // Collection of beginning functions ex. Welcome(); CustEmployeeSwitch(); ect...               
+                }
+            }
+
+            public void PasswordPath()
+            {
+                GetPassword();
+                CheckPassword();
+            }
+
+            public string GetPassword()
+            {
+                Console.WriteLine("Please enter the employee password now. Password is case sensitive.");
+                return password = Console.ReadLine();
+            }
+
+            public void CheckPassword()
+            {
+                if (password == empPassword)
+                {
+                    //EmployeePath();
+                }
+            }
+
+
+        }
     }
-}
