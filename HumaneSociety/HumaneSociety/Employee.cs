15aacb9fba1a38bcba80041b1d1c63e7c325b605
Corey Rome <coreyrome2@gmail.com>
2017-12-20 15:41:43 -0600

    employee class and comments added

---
+++ HumaneSociety/HumaneSociety/Employee.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class Employee
+    {
+
+        // collect money
+
+        //check shot status of animal
+
+        // give animal its shot
+
+        //set status of animal to "adopted"
+
+        // check room of an animal
+
+        // check animals food info. "amout(per week), what type
+    }
+}
