a89252b5959f07eff2d5a0bd9a69aaf5b1387f0d
David Merkel <merkel_david@yahoo.com>
2017-12-20 11:38:12 -0600

    beginner db stuff

--- HumaneSociety/HumaneSociety/DB.cs
+++ HumaneSociety/HumaneSociety/DB.cs
@@ -3,10 +3,54 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Data.SqlClient;
 
 namespace HumaneSociety
 {
     class DB
     {
+        private SqlConnection seed = new SqlConnection("Data Source = (LocalDB)\\MSSQLLocalDB; AttachDbFilename = C:\\Program files\\MicosoftSQLServer\\MSSQL14.MSSQLSERVER\\MSSQL\\DATA\\HumanSociety.mdf; Integrated Security = True");
+
+        public void addTodataBase(string name, double profit)
+        {
+            seed.Open();
+
+            SqlCommand command = seed.CreateCommand();
+            SqlTransaction transaction;
+
+            transaction = seed.BeginTransaction();
+
+            //establishing connection to db
+            command.Connection = seed;
+            command.Transaction = transaction;
+
+            //write sql statment
+            command.CommandText = "INSERT INTO Pets (Name,RoomNumber,vaccinated,type) VALUES ('" + name + "'," + profit + ") ";
+
+            //preparing the SQL statment
+            command.ExecuteNonQuery();
+
+            //Executing the statement
+            transaction.Commit();
+
+        }
+
+        public void getHighScores()
+        {
+
+            seed.Open();
+
+            SqlCommand command = new SqlCommand("SELECT TOP 10 name,score FROM Scores ORDER BY score DESC", seed);
+            SqlDataReader reader = command.ExecuteReader();
+            Console.WriteLine("HIGH SCORES");
+            Console.WriteLine("-----------");
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(0) + "\t" + reader.GetDouble(1));
+            }
+            Console.WriteLine("");
+
+        }
+
     }
 }
