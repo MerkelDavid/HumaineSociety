2affeaf991aa5ba303206b132ad4fefea12ef746
Corey Rome <coreyrome2@gmail.com>
2017-12-22 13:36:19 -0600

    added petvalues class, edited csv reader class

--- HumaneSociety/HumaneSociety/CSVReader.cs
+++ HumaneSociety/HumaneSociety/CSVReader.cs
@@ -9,30 +9,10 @@ namespace HumaneSociety
 {
     class CSVReader
     {
-        List<DailyValues> values = File.ReadAllLines("C:\Users\Cortana\Documents\Visual Studio 2015\Projects\HumaneSociety(Group)\HumaneSociety\HumaneSociety")
-                                           .Skip(1)
-                                           .Select(v => DailyValues.FromCsv(v))
+        PetValues petvalues;
+        List<PetValues> values = File.ReadAllLines("C:\Users\Cortana\Documents\Visual Studio 2015\Projects\HumaneSociety(Group)\HumaneSociety\HumaneSociety")
+                                           .Select(v => PetValues.FromCsv(v))
                                            .ToList();
     }
 }
 
-class DailyValues
-{    
-    string type;
-    string name;
-    string size;
-    bool vaccinated;
-    
-
-    public static DailyValues FromCsv(string csvLine)
-    {
-        string[] values = csvLine.Split(',');
-        DailyValues dailyValues = new DailyValues();
-        dailyValues.type = Convert.ToString(values[0]);
-        dailyValues.name = Convert.ToString(values[1]);
-        dailyValues.size = Convert.ToString(values[2]);
-        dailyValues.vaccinated = Convert.ToBoolean(values[3]);
-        return dailyValues;
-    }
-}
-}
---
+++ HumaneSociety/HumaneSociety/PetValues.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HumaneSociety
+{
+    class PetValues
+    {
+        string type;
+        string name;
+        string size;
+        bool vaccinated;
+
+
+        public static PetValues FromCsv(string csvLine)
+        {
+            string[] values = csvLine.Split(',');
+            PetValues dailyValues = new PetValues();
+            dailyValues.type = Convert.ToString(values[0]);
+            dailyValues.name = Convert.ToString(values[1]);
+            dailyValues.size = Convert.ToString(values[2]);
+            dailyValues.vaccinated = Convert.ToBoolean(values[3]);
+            return dailyValues;
+        }
+    }
+}
